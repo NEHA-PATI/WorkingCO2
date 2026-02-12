9e0b666de58e74b615a591fd14a03c8dbbb3052f
Soumik Basu <soumikbasu2003@gmail.com>
2026-02-12 16:54:36 +0530

    Update mail service

--- backend/authentication/utils/sendOTP.js
+++ backend/authentication/utils/sendOTP.js
@@ -1,29 +1,3 @@
-// const { Resend } = require("resend");
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-// const sendOTP = async (email, otp) => {
-//   const htmlContent = `
-//     <div style="font-family:Arial, sans-serif; padding:20px;">
-//         <h2>Your OTP Code</h2>
-//         <p>Hello 👋,</p>
-//         <p>Your One-Time Password (OTP) is:</p>
-//         <h3 style="color:#2b6cb0;">${otp}</h3>
-//         <p>This code will expire in 10 minutes.</p>
-//         <br/>
-//         <p>— The Team</p>
-//       </div>
-//   `;
-
-//   await resend.emails.send({
-//     from: "Carbon Positive <onboarding@resend.dev>",
-//     to: "soumikbasu2003@gmail.com",        // ✅ Hardcoded now 
-//     subject: "Your OTP Code",
-//     html: htmlContent,
-//   });
-// };
-
-// module.exports = sendOTP;
-
 
 require("dotenv").config();
 const axios = require("axios");
