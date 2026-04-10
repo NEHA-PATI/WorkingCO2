a44bd7fdf29344323e19643e2d8d968b9863ecda
NEHA-PATI <nehapati11122004@gmail.com>
2026-04-10 12:03:18 +0530

    frontend CI resolve error

--- frontend/Web_GoCarbonPositive/eslint.config.js
+++ frontend/Web_GoCarbonPositive/eslint.config.js
@@ -7,6 +7,16 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist']),
   {
+    files: [
+      'tailwind.config.js',
+      'tailwind.config.cjs',
+    ],
+    languageOptions: {
+      sourceType: 'script',
+      globals: globals.node,
+    },
+  },
+  {
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
--- frontend/Web_GoCarbonPositive/tailwind.config.js
+++ frontend/Web_GoCarbonPositive/tailwind.config.js
@@ -1,4 +1,3 @@
-/* global require, module */
 const config = require("./tailwind.config.cjs");
 
 module.exports = config;
